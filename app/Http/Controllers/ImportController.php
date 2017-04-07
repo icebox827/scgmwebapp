5a9b7ff76d177e9bf7411825208e105c2669421e
Castro Alhdo <castroalhdo@gmail.com>
2017-04-07 17:45:02 -0500

    Csv import

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -3,16 +3,43 @@
 namespace App\Http\Controllers;
 
 use App\Commune;
+use App\Proprietaire;
+use App\Vehicule;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
 
 class ImportController extends Controller
 {
     public function station(Request $request){
-//        $st = json_decode($station);
-//        $commune = Commune::where('name', 'Delmas')->first();
-//
-//        return var_dump($st);
-        return Input::get('commune');
+        $array = $this->readCsv();
+        for($i=0;$i<count($array);$i++){
+            $vehicule = new Vehicule();
+            $vehicule->nummoteur=$array[$i][1];
+            $vehicule->numserie = $array[$i][2];
+            $vehicule->marque = $array[$i][3];
+            $vehicule->modele = $array[$i][4];
+            $vehicule->annee = $array[$i][5];
+            $vehicule->couleur = $array[$i][6];
+            $vehicule->immatriculation = $array[$i][7];
+            $vehicule->puissance = $array[$i][11];
+            $proprietaire = Proprietaire::where('nif',$array[$i][12])->first();
+            if($proprietaire){
+                $vehicule->proprietaire_id = $proprietaire->id;
+                $vehicule->save();
+            }
+
+        }
+
+
+    }
+
+    public function readCsv(){
+        $file_handle = fopen('/var/www/scgm/public/vehicule.csv','r');
+        while (!feof($file_handle)){
+            $line_of_text[] = fgetcsv($file_handle, 1024,";");
+        }
+        fclose($file_handle);
+
+        return $line_of_text;
     }
 }
