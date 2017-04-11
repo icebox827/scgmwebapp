fa80ff628155943e2e7c4b1c1b863940afcfe8c5
Castro Alhdo <castroalhdo@gmail.com>
2017-04-11 14:18:15 -0500

    Last modif

--- app/Http/Controllers/RegistraireController.php
+++ app/Http/Controllers/RegistraireController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Station;
 use Illuminate\Http\Request;
 
 class RegistraireController extends Controller
 {
     public function index(){
-        return view('registraire');
+        $stations = Station::orderBy('name','asc')->get();
+        return view('registraire',compact('stations'));
+    }
+    public function save(Request $request){
+        $this->validate($request,[
+
+        ]);
     }
 
 }
